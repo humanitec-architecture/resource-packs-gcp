ddb5e38d1e3bda3b1aa418b579557354c7dbde33
Johannes Würbach <johannes.wuerbach@googlemail.com>
2023-12-22 17:56:56 +0100

    feat: iterate

--- examples/gcs/main.tf
+++ examples/gcs/main.tf
@@ -48,25 +48,24 @@ resource "humanitec_resource_definition_criteria" "gcs_basic" {
   class                  = local.gcs_class
 }
 
-# TODO
-# module "iam_policy_s3_admin" {
-#   source = "../../humanitec-resource-defs/iam-policy/s3-admin"
+module "iam_role_binding_gcs_admin" {
+  source = "../../humanitec-resource-defs/iam-role-binding/gcs-admin"
 
-#   access_key             = var.access_key
-#   secret_key             = var.secret_key
-#   resource_packs_aws_rev = var.resource_packs_aws_rev
-#   humanitec_organization = var.humanitec_organization
-#   region                 = var.region
+  project     = var.project
+  credentials = var.credentials
 
-#   prefix                       = local.res_def_prefix
-#   s3_resource_definition_class = local.s3_class
-# }
+  resource_packs_gcp_url = var.resource_packs_gcp_url
+  resource_packs_gcp_rev = var.resource_packs_gcp_rev
 
-# resource "humanitec_resource_definition_criteria" "iam_policy_s3_admin" {
-#   resource_definition_id = module.iam_policy_s3_admin.id
-#   app_id                 = humanitec_application.example.id
-#   class                  = local.s3_admin_policy_class
-# }
+  prefix                        = local.res_def_prefix
+  gcs_resource_definition_class = local.gcs_class
+}
+
+resource "humanitec_resource_definition_criteria" "iam_role_binding_gcs_admin" {
+  resource_definition_id = module.iam_role_binding_gcs_admin.id
+  app_id                 = humanitec_application.example.id
+  class                  = local.gcs_admin_policy_class
+}
 
 module "gcp_service_account_workload" {
   source = "../../humanitec-resource-defs/gcp-service-account/workload"
