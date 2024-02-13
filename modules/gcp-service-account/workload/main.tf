7eb89f7456acffb0f123b423ed7833e983a82563
Johannes Würbach <johannes.wuerbach@googlemail.com>
2024-02-13 18:13:30 +0100

    fix: create service-account only when needed

--- modules/gcp-service-account/workload/main.tf
+++ modules/gcp-service-account/workload/main.tf
@@ -1,8 +1,11 @@
 locals {
   k8s_service_account_name = "${var.app_id}-${var.env_id}-${trimprefix(var.res_id, "modules.")}"
+  account_required         = length(var.bindings) + length(var.roles) > 0
 }
 
 resource "google_service_account" "main" {
+  count = local.account_required ? 1 : 0
+
   display_name = "${var.prefix}workload service account"
   account_id   = "${var.prefix}workload"
 }
@@ -12,11 +15,13 @@ resource "google_project_iam_member" "role" {
 
   project = var.project
   role    = each.key
-  member  = "serviceAccount:${google_service_account.main.email}"
+  member  = "serviceAccount:${google_service_account.main[0].email}"
 }
 
 resource "google_service_account_iam_member" "workload_identity_k8s_service_account" {
-  service_account_id = google_service_account.main.name
+  count = local.account_required ? 1 : 0
+
+  service_account_id = google_service_account.main[0].name
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${var.project}.svc.id.goog[${var.namespace}/${local.k8s_service_account_name}]"
 }
